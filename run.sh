188a6bfd214dda8684f6149f29901ae742a98434
rongjun.xie <rongjun.xie@hotmail.com>
2019-02-06 15:24:54 +1100

    Impove performance with gunicorn and gevent

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Start the server (Normal Mode)......."
-FLASK_APP=./EATest/main.py flask run
+#FLASK_APP=./EATest/main.py flask run
+
+cd EATest
+gunicorn main:app -w 4 -b 127.0.0.1:5000 -k gevent
